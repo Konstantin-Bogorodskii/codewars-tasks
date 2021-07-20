09cd7cb7607c73f4d20a98069d186b0a607f984d
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-07-20 23:33:10 +0300

    solved 2 tasks

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// **** Task-1 ****
+// Condition:
+// Valid smiley face examples:
+// :) :D ;-D :~)
+// Invalid smiley faces:
+// ;( :> :} :]
+//
+// Solution:
+// function countSmileys(arr) {
+//   return arr.filter(v=>v.match(/(:|;)(-|~)?(\)|D)/)).length
+// }
+// countSmileys([':)', ';(', ';}', ':-D']);       // ==> return 2;
+// countSmileys([';D', ':-(', ':-)', ';~)']);     // ==> return 3;
+// countSmileys([';]', ':[', ';*', ':$', ';-D']); //  ==> return 1;
+
+// **** Task-2 ****
+function solution(str, ending) {
+   return str.endsWith(ending);
+}
+
+console.log(solution('abc', 'bc'));
+console.log(solution('abcde', 'abc'));
+console.log(solution('sumo', 'omo'));
