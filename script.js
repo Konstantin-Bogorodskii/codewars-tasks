81ca7d487d2c8e9e4dd8ae3c4cd5a96cb1e1f79c
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-07-23 00:52:00 +0300

    resolve 3 tasks

--- script.js
+++ script.js
@@ -25,4 +25,50 @@
 // console.log(solution('sumo', 'omo'));
 
 // **** Task-3****
-console.log('1');
+// Отсортировать от меньшего кол-ва букв к большему
+// function sortByLength(array) {
+//    return array.sort((a, b) => {
+//       return a.length - b.length;
+//    });
+// }
+// console.log(sortByLength(['Beg', 'Life', 'I', 'To']));
+
+// **** Task-4****
+// function findSum(n) {
+//   let sum = 0;
+//    for (let i = 0; i <= n; i++) {
+//       if (i % 3 == 0 || i % 5 == 0) {
+//          sum += i;
+//       }
+//    }
+//   return sum;
+// }
+// findSum(5);
+// **** Task-5****
+// function oddOrEven(array) {
+//    if (array.length > 0) {
+//       let b = array.reduce((accum, elem) => accum + elem);
+//       if (b % 2 === 0) {
+//          return 'even';
+//       }
+//       return 'odd';
+//    } else if (array.length == 0) {
+//       return 'even';
+//    }
+// }
+// console.log(oddOrEven([0, 3, -4]));
+
+// **** Task-6****
+// function getSum(a, b) {
+//    let max = Math.max(a, b);
+//    let min = Math.min(a, b);
+//    let sum = 0;
+//    if (a != b) {
+//       for (let i = min; i < max + 1; i++) {
+//          sum += i;
+//       }
+//       return sum;
+//    } else if (a == b) {
+//       return a;
+//    }
+// }
